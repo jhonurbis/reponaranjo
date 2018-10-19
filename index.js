86b9f239a8555ac83542934011f2735116d7ff11
jhonurbis <44198349+jhonurbis@users.noreply.github.com>
2018-10-19 17:22:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-var request = require('request');
-var epress = require('express');
-var bodyParser = require('body-parser');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-var path = require("path");
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
 
+app.get('/version', (req, res) => {
+    res.status(200).send("APIAI Webhook Integration. Version 1.0");
+});
+
+app.get('/', (req, res) => {
+    res.status(200).send("Hello from APIAI Webhook Integration.");
+});
 
 /* Handling all messenges */
-app.post('/webhook', function (req, res) => {
-    console.log('rcibiii');
-    res.setHeader('content-type', 'application/json');
-    var city = req.body.queryResult.parameters['geo-city'];
-	let response = "holaaa";
-	let responseObj = {
-					"fulfillmentText":response,
-					 "fulfillmentMessages": [
-        {
-            "text": {
-                "text": [
-                    "fulfillmentMessages text attribute"
-                ]
-            }
-        }
-    ],		"source":""
-	
-			
-					
-				}
-		  
-		  return res.json(responseObj);
-})
+app.post('/webhook', (req, res) => {
+    console.log(req.body);
+    console.log(req.body.result.parameters["geo-city"]);
+    console.log(req.body.result.parameters["comments"]);
+    console.log(req.body.result.parameters["resort-location"]);
+    //Persist this in some database
+    //Send out an email that new feedback has come in
+    res.status(200).json({
+          speech: "Thank you for the feedback",
+          displayText: "Thank you for the feedback",
+          source: 'Hotel Feedback System'});
+});
+
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
