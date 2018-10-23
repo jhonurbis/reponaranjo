8d364393ed9bdc641214657867814f8007efbfd1
jhonurbis <44198349+jhonurbis@users.noreply.github.com>
2018-10-23 13:27:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.get('/', (req, res) => {
 app.post('/webhook', (req, res) => {
     console.log(req.body);
     console.log(req.body.result.parameters["geo-city"]);
+    var city = req.body.result.parameters["geo-city"];
     //Persist this in some database
     //Send out an email that new feedback has come in
     res.status(200).json({
