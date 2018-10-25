6c15e8d0e887d80ea39a32329c962ebeec8d1561
jhonurbis <44198349+jhonurbis@users.noreply.github.com>
2018-10-25 11:43:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,9 +23,11 @@ app.post('/webhook', (req, res) => {
 
   
   request(url, function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+  let respose = JSON.parse(body);
+  console.log('respuesta', response);
+ //console.log('error:', error); // Print the error if one occurred
+  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //console.log('body:', body); // Print the HTML for the Google homepage.
 });
     
     
