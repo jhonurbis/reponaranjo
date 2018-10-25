44c2202b611bfa830cd5210cf7d80a8d7f12ba98
jhonurbis <44198349+jhonurbis@users.noreply.github.com>
2018-10-25 12:29:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,22 +19,13 @@ app.post('/webhook', (req, res) => {
     console.log(req.body);
     console.log(req.body.queryResult.parameters["geo-city"]);
     var city = req.body.queryResult.parameters["geo-city"];// json que envia dialogflow
-  var url = `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=174d1966288642ebb61162416181710&q=${city}&format=json&num_of_days=1`;
+	//var url = `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=174d1966288642ebb61162416181710&q=${city}&format=json&num_of_days=1`;
 
   
-  request(url, function (error, response, body) {
-  let resJson = JSON.parse(body);
-  let temperatura = resJson['data']['weather'][0]['maxtempC'];
-      
- //let location = response['data']['request'][0];
-  console.log('tempe', temperatura);
- //console.log('error:', error); // Print the error if one occurred
-  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body); // Print the HTML for the Google homepage.
-});
     
     
-    var urlApi = `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=174d1966288642ebb61162416181710&q=${city}&format=json&num_of_days=1`;
+    
+    //var urlApi = `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=174d1966288642ebb61162416181710&q=${city}&format=json&num_of_days=1`;
       
     // let url = `http://api.openweathermap.org/data/2.5/forecast?q=${ubicacion}&APPID=apikey`;
    // console.log("url-api"+urlApi);
@@ -42,16 +33,30 @@ app.post('/webhook', (req, res) => {
     
     //Persist this in some database
     //Send out an email that new feedback has come in
+    getTiempo();
     res.status(200).json({
-       fulfillmentText: "Temperatura Maxima en "+city + "sera de " + temperatura,
+       fulfillmentText: "Temperatura Maxima en "+city + "sera de ",
         source: 'timpo Feedback System'});
-    
-      
-    
-
     
     
 });
+
+function getTiempo(){
+	var url = `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=174d1966288642ebb61162416181710&q=${city}&format=json&num_of_days=1`;
+
+  request(url, function (error, response, body) {
+  let resJson = JSON.parse(body);
+  let temperatura = resJson['data']['weather'][0]['maxtempC'];
+      
+  console.log('tempe', temperatura);
+ //console.log('error:', error); // Print the error if one occurred
+  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //console.log('body:', body); // Print the HTML for the Google homepage.
+});
+
+return resJson;
+
+}
 
 const server = app.listen(process.env.PORT || 5000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
