59077f9f08e6992d259e20a3f3e9544528a3130f
jhonurbis <44198349+jhonurbis@users.noreply.github.com>
2018-10-25 13:26:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,6 @@ app.post('/webhook', (req, res) => {
     var city = req.body.queryResult.parameters["geo-city"];// json que envia dialogflow
 	//var url = `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=174d1966288642ebb61162416181710&q=${city}&format=json&num_of_days=1`;
 
-  	respuesta=getTiempo(city);
-	console.log("url-api"+respuesta);
     
     
     
@@ -40,34 +38,6 @@ app.post('/webhook', (req, res) => {
     
     
 });
-
-
-
-
-
-var result;
-
-
-
-  function cb (error, response, body) {
-  let resJson = JSON.parse(body);
-  let temperatura = resJson['data']['weather'][0]['maxtempC'];
-  result=resJson['data']['weather'][0]['maxtempC'];
-      
-  console.log('tempe', temperatura);
-   console.log('error:', error); // Print the error if one occurred
-  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body); // Print the HTML for the Google homepage.
-}
-
-
-function getTiempo(ciudad){
-	
-	var url = `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=174d1966288642ebb61162416181710&q=${ciudad}&format=json&num_of_days=1`;
-	result=request(url,cb);
-	return result;
-}
-
 
 const server = app.listen(process.env.PORT || 5000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
