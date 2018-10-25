5846b69fa6bc8f590e2dd95ff5d254626358de1a
jhonurbis <44198349+jhonurbis@users.noreply.github.com>
2018-10-25 13:21:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -46,6 +46,21 @@ app.post('/webhook', (req, res) => {
 
 
 var result;
+
+
+
+  function cb (error, response, body) {
+  let resJson = JSON.parse(body);
+  let temperatura = resJson['data']['weather'][0]['maxtempC'];
+	  result=temperatura;
+      
+  console.log('tempe', temperatura);
+   console.log('error:', error); // Print the error if one occurred
+  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //console.log('body:', body); // Print the HTML for the Google homepage.
+}
+
+
 function getTiempo(ciudad){
 	
 	var url = `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=174d1966288642ebb61162416181710&q=${ciudad}&format=json&num_of_days=1`;
@@ -53,15 +68,6 @@ function getTiempo(ciudad){
 	return result;
 }
 
-  function cb (error, response, body) {
-  let resJson = JSON.parse(body);
-  let temperatura = resJson['data']['weather'][0]['maxtempC'];
-      
-  console.log('tempe', temperatura);
-   console.log('error:', error); // Print the error if one occurred
-  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body); // Print the HTML for the Google homepage.
-}
 
 const server = app.listen(process.env.PORT || 5000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
