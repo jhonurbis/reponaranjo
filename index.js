09cd2c12a7aa51eac218f2aee2e37255e7f5e8fe
jhonurbis <44198349+jhonurbis@users.noreply.github.com>
2018-10-25 09:41:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.post('/webhook', (req, res) => {
     //Persist this in some database
     //Send out an email that new feedback has come in
     res.status(200).json({
-       fulfillmentText: "la ciudad introducida es",
+       fulfillmentText: "la ciudad introducida es "+city,
         source: 'Hotel Feedback System'});
 
     
